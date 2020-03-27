16bbd29ebb7e1e02471d63d2f58834507c678b3e
Paul Nissen <nissenbrau@gmail.com>
2020-03-27 12:46:09 -0600

    built ShoppingListService object, wrote first test in shopping-list-spec

--- src/shopping-list-service.js
+++ src/shopping-list-service.js
@@ -1,12 +1,38 @@
 const ShoppingListService = {
   // get all items
-
+  getAllItems(knex) {
+    return knex.select('*').from('shopping_list')
+  },
+  // get item by id
+  getById(knex, id) {
+    return knex('shopping_list')
+      .select('*')
+      .where({ id })
+      .first()
+  },
 
   // insert item
-
+  insertItem(knex, newItem) {
+    return knex
+      .insert(newItem)
+      .into('shopping_list')
+      .returning('*')
+      .then(rows => rows[0])
+  },
 
   // update item
-
+  updateItem(knex, id, updatedItemFields) {
+    return knex('shopping_list')
+      .where({ id })
+      .update(updatedItemFields)
+  },
 
   // delete item
+  deleteItem(knex, id) {
+    return knex('shopping_list')
+      .where({ id })
+      .delete()
+  },
 }
+
+module.exports = ShoppingListService;
--- test/shopping-list-service.spec.js
+++ test/shopping-list-service.spec.js
@@ -1,20 +1,77 @@
-const ArticlesService = require('../src/articles-service');
+const ShoppingListService = require('../src/shopping-list-service');
 const knex = require('knex');
 
 // describe test suite
-
-  // create dummy items for test table
+describe(`Shopping list service object`, function() {
+  let db
+  
+  // prepare mock data to insert into test table
+  let listItems = [
+    {
+      id: 1,
+      name: 'dog food',
+      price: 30.00, 
+      category: 'Main', 
+      checked: true,
+      date_added: new Date('2029-01-22T16:28:32.615Z')
+    },
+    {
+      id: 2,
+      name: 'gold bears',
+      price: 1.25, 
+      category: 'Snack', 
+      checked: false,
+      date_added: new Date('2100-05-22T16:28:32.615Z')
+    },
+    {
+      id: 3,
+      name: 'dozen eggs',
+      price: 2.00, 
+      category: 'Breakfast', 
+      checked: false,
+      date_added: new Date('1919-12-22T16:28:32.615Z')
+    },
+  ]
 
   // create knex instance before and add test table connection path to .env file
+  before(() => {
+    db = knex({
+      client: 'pg',
+      connection: process.env.TEST_DB_URL,
+    });
+  });
 
-  // write before/after/afterEach code for tests to start fresh and clean up after
+  // write before/afterEach code for tests to start with empty table and clean up test data after
+  before(() => db('shopping_list').truncate());
+
+  afterEach(() => db('shopping_list').truncate());
+
+  // write after code to disconnect from database when finished
+  after(() => db.destroy());
 
   // write tests for context: given table has existing data
-
-    // populate table with test data
+  context(`Given 'shopping_list' table contains data`, () => {
+     // populate table with test data  
+    beforeEach(() => {
+      return db 
+        .insert(listItems)
+        .into('shopping_list')
+    });
 
     // get all items
+    it(`getAllItems() resolves all items from 'shopping_list' table`, () => {
+          return ShoppingListService.getAllItems(db)
+            .then(actual => {
+              expect(actual).to.eql(listItems);
+            });
+    });
 
+
+    
+  })
+
+   
+    
     // get an item by id
 
     // delete an item
@@ -24,3 +81,6 @@ const knex = require('knex');
     // get empty array of data
 
     // insert a row of data
+
+})
+  
